903fda56cee20ebe3c08d2381d31cdea0f5c17f8
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2023-08-30 17:45:24 +0300

    Implemented Selection Sort

---
+++ C#/SortingAlgorithms.cs
@@ -0,0 +1,64 @@
+﻿namespace GrokkingAlgorithms
+{
+    namespace CSharp
+    {
+        internal static class SortingAlgorithms
+        {
+            internal enum Order : byte
+            {
+                Ascending,
+                Descending
+            }
+
+            private static void Swap<T>(T[] array, int index1, int index2)
+            {
+                T temp = array[index1];
+                array[index1] = array[index2];
+                array[index2] = temp;
+            }
+
+            public static void SelectionSort<T>(T[] array, Order order) where T : IComparable<T> 
+            {
+                int index;
+
+                switch (order)
+                {
+                    case Order.Ascending:
+                        {
+                            for (int i = 0; i < array.Length; ++i)
+                            {
+                                index = i;
+
+                                for (int j = i + 1; j < array.Length; ++j)
+                                {
+                                    if (array[i].CompareTo(array[j]) > 0)
+                                        index = j;
+                                }
+
+                                if (i != index)
+                                    Swap(array, index, i);
+                            }
+                        }
+                        break;
+                    case Order.Descending:
+                        {
+                            for (int i = 0; i < array.Length; ++i)
+                            {
+                                index = i;
+
+                                for (int j = i + 1; j < array.Length; ++j)
+                                {
+                                    if (array[i].CompareTo(array[j]) < 0)
+                                        index = j;
+                                }
+
+                                if (i != index)
+                                    Swap(array, index, i);
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
