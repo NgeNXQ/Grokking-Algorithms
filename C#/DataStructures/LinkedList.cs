45190e287ec569a5154ed10ee91b66316367d8a2
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2023-09-01 22:08:18 +0300

    LinkedList first commit

---
+++ C#/DataStructures/LinkedList.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("GrokkingAlgorithms_CSharp.UnitTests")]
+
+namespace GrokkingAlgorithms
+{
+    namespace CSharp
+    {
+        namespace DataStructures
+        {
+            internal sealed class LinkedList<T> : IEnumerable<T>
+            {
+                private int count;
+                private LinkedListNode<T> head;
+                private LinkedListNode<T> tail;
+
+                public LinkedListNode<T> First { get => this.head; }
+
+                public LinkedListNode<T> Last { get => this.tail; }
+
+                public int Count { get => this.count; }
+
+                public bool IsEmpty { get => this.count < 1; }
+
+                public LinkedList()
+                {
+                    this.count = 0;
+                    this.head = null;
+                    this.tail = null;
+                }
+
+                public LinkedList(T value) : this() => this.AddLast(value);
+
+                public LinkedList(IEnumerable<T> collection) : this()
+                {
+                    foreach(T value in collection)
+                        this.AddLast(value);
+                }
+
+                public LinkedListNode<T> AddFirst(T value)
+                {
+                    LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+
+                    if (this.count < 1)
+                        this.tail = newNode;
+
+                    newNode.Next = this.head;
+                    this.head = newNode;
+                    ++this.count;
+                    return newNode;
+                }
+
+                public void AddFirst(LinkedListNode<T> node)
+                {
+                    if (node == null)
+                        throw new ArgumentNullException($"{nameof(node)} is null.");
+
+                    if (this.count < 1)
+                        this.tail = node;
+
+                    node.Next = this.head;
+                    this.head = node;
+                    ++this.count;
+                }
+
+                public LinkedListNode<T> AddLast(T value)
+                {
+                    LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+                    
+                    if (this.count < 1)
+                    {
+                        this.head = this.tail = newNode;
+                    }
+                    else
+                    {
+                        this.tail.Next = newNode;
+                        this.tail = newNode;
+                    }
+                    
+                    ++this.count;
+                    return newNode;
+                }
+
+                public void AddLast(LinkedListNode<T> node)
+                {
+                    if (node == null)
+                        throw new ArgumentNullException($"{nameof(node)} is null.");
+
+                    if (this.count < 1)
+                    {
+                        this.head = this.tail = node;
+                    }
+                    else
+                    {
+                        this.tail.Next = node;
+                        this.tail = node;
+                    }
+
+                    ++this.count;
+                }
+
+                public LinkedListNode<T> AddBefore(LinkedListNode<T> node, T value)
+                {
+                    if (node == null)
+                        throw new ArgumentNullException($"{nameof(node)} is null.");
+
+                    if (head == node)
+                        return this.AddFirst(value);
+
+                    LinkedListNode<T> previous = head;
+                    LinkedListNode<T> current = head.Next;
+                    LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+
+                    while (current != null)
+                    {
+                        if (current == node)
+                        {
+                            newNode.Next = previous.Next;
+                            previous.Next = newNode;
+                            ++count;
+                            return newNode;
+                        }
+
+                        previous = current;
+                        current = current.Next;
+                    }
+
+                    throw new System.InvalidOperationException($"{nameof(LinkedList<T>)} does not contain specific {nameof(node)}.");
+                }
+
+                public void AddBefore(LinkedListNode<T> node, LinkedListNode<T> newNode)
+                {
+                    if (node == null)
+                        throw new ArgumentNullException($"{nameof(node)} is null.");
+
+                    if (head == node)
+                    {
+                        this.AddFirst(newNode);
+                        return;
+                    }
+
+                    LinkedListNode<T> previous = head;
+                    LinkedListNode<T> current = head.Next;
+
+                    while (current != null)
+                    {
+                        if (current == node)
+                        {
+                            newNode.Next = previous.Next;
+                            previous.Next = newNode;
+                            ++count;
+                            return;
+                        }
+
+                        previous = current;
+                        current = current.Next;
+                    }
+
+                    throw new System.InvalidOperationException($"{nameof(LinkedList<T>)} does not contain specific {nameof(node)}.");
+                }
+
+                //AddAfter(LinkedListNode<T> node, T value): Adds a new node with the specified value after the specified existing node.
+
+                //Remove(LinkedListNode<T> node): Removes the specified node from the LinkedList.
+
+                //Find(T value): Finds the first occurrence of a node with the specified value in the LinkedList.
+
+                //Contains(T value): Determines whether the LinkedList contains a node with the specified value.
+
+                //ToArray(): Copies the elements of the LinkedList to a new array.
+
+                //public LinkedListNode<T> RemoveFirst()
+                //{
+
+                //}
+
+                //public LinkedListNode<T> RemoveLast()
+                //{
+
+                //}
+
+                //public void Clear()
+                //{
+                //    this.head = null;
+                //    this.tail = null;
+                //    this.count = 0;
+                //}
+
+                public IEnumerator<T> GetEnumerator()
+                {
+                    LinkedListNode<T> node = this.head;
+
+                    while (node != null)
+                    {
+                        yield return node.Value;
+                        node = node.Next;
+                    }
+                }
+
+                IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+            }
+
+            internal sealed class LinkedListNode<T>
+            {
+                public T Value { get; set; }
+
+                public LinkedListNode<T> Next { get; set; }
+
+                public LinkedListNode()
+                {
+                    this.Value = default;
+                    this.Next = null;
+                }
+
+                public LinkedListNode(T value) : this() => this.Value = value;
+            }
+        }   
+    }
+}
---
+++ C#/GrokkingAlgorithms_CSharp.UnitTests/LinkedListTests.cs
@@ -0,0 +1,222 @@
+﻿using NUnit.Framework;
+
+namespace GrokkingAlgorithms
+{
+    namespace CSharp
+    {
+        namespace DataStructures
+        {
+            namespace UnitTests
+            {
+                [TestFixture]
+                public sealed class LinkedListTests
+                {
+                    private LinkedList<int> list;
+
+                    [SetUp]
+                    public void SetUp() => list = new LinkedList<int>();
+
+                    #region AddFirst
+
+                    [Test]
+                    public void AddFirst_DefaultValue_FirstValueIsCorrect()
+                    {
+                        int expected = 5;
+
+                        list.AddFirst(1);
+                        list.AddFirst(2);
+                        list.AddFirst(3);
+                        list.AddFirst(4);
+                        list.AddFirst(5);
+
+                        Assert.That(list.First.Value == expected);
+                    }
+
+                    [Test]
+                    public void AddFirst_LinkedListNode_FirstValueIsCorrect()
+                    {
+                        int expected = 5;
+
+                        list.AddFirst(new LinkedListNode<int>(1));
+                        list.AddFirst(new LinkedListNode<int>(2));
+                        list.AddFirst(new LinkedListNode<int>(3));
+                        list.AddFirst(new LinkedListNode<int>(4));
+                        list.AddFirst(new LinkedListNode<int>(5));
+
+                        Assert.That(list.First.Value == expected);
+                    }
+
+                    [Test]
+                    public void AddFirst_Null_ThrowsArgumentNullException() 
+                        => Assert.Throws<ArgumentNullException>(() => list.AddFirst(null));
+
+                    [Test]
+                    public void AddFirst_ValuesAndLinkedListNodes_ReturnsCount()
+                    {
+                        int expected = 10;
+
+                        list.AddLast(1);
+                        list.AddLast(new LinkedListNode<int>(1));
+                        list.AddLast(2);
+                        list.AddLast(new LinkedListNode<int>(2));
+                        list.AddLast(3);
+                        list.AddLast(new LinkedListNode<int>(3));
+                        list.AddLast(4);
+                        list.AddLast(new LinkedListNode<int>(4));
+                        list.AddLast(5);
+                        list.AddLast(new LinkedListNode<int>(5));
+
+                        Assert.That(expected, Is.EqualTo(list.Count));
+                    }
+
+                    #endregion
+
+                    #region AddLast
+
+                    [Test]
+                    public void AddLast_DefaultValue_LastValueIsCorrect()
+                    {
+                        int expected = 5;
+
+                        list.AddLast(1);
+                        list.AddLast(2);
+                        list.AddLast(3);
+                        list.AddLast(4);
+                        list.AddLast(5);
+
+                        Assert.That(list.Last.Value == expected);
+                    }
+
+                    [Test]
+                    public void AddLast_LinkedListNode_LastValueIsCorrect()
+                    {
+                        int expected = 5;
+
+                        list.AddLast(new LinkedListNode<int>(1));
+                        list.AddLast(new LinkedListNode<int>(2));
+                        list.AddLast(new LinkedListNode<int>(3));
+                        list.AddLast(new LinkedListNode<int>(4));
+                        list.AddLast(new LinkedListNode<int>(5));
+
+                        Assert.That(list.Last.Value == expected);
+                    }
+
+                    [Test]
+                    public void AddLast_Null_ThrowsArgumentNullException()
+                        => Assert.Throws<ArgumentNullException>(() => list.AddLast(null));
+
+                    [Test]
+                    public void AddLast_ValuesAndLinkedListNodes_ReturnsCount()
+                    {
+                        int expected = 10;
+
+                        list.AddLast(1);
+                        list.AddLast(new LinkedListNode<int>(1));
+                        list.AddLast(2);
+                        list.AddLast(new LinkedListNode<int>(2));
+                        list.AddLast(3);
+                        list.AddLast(new LinkedListNode<int>(3));
+                        list.AddLast(4);
+                        list.AddLast(new LinkedListNode<int>(4));
+                        list.AddLast(5);
+                        list.AddLast(new LinkedListNode<int>(5));
+
+                        Assert.That(expected, Is.EqualTo(list.Count));
+                    }
+
+                    #endregion
+
+                    #region AddBefore
+
+                    [Test]
+                    public void AddBefore_LinkedListNodesAndValues_ReturnsCount()
+                    {
+                        int expected = 10;
+
+                        LinkedListNode<int> node1 = list.AddLast(1);
+                        LinkedListNode<int> node2 = list.AddLast(2);
+                        LinkedListNode<int> node3 = list.AddLast(3);
+                        LinkedListNode<int> node4 = list.AddLast(4);
+                        LinkedListNode<int> node5 = list.AddLast(5);
+
+                        list.AddBefore(node1, 1);
+                        list.AddBefore(node2, 2);
+                        list.AddBefore(node3, 3);
+                        list.AddBefore(node4, 4);
+                        list.AddBefore(node5, 5);
+
+                        Assert.That(list.Count, Is.EqualTo(expected));
+                    }
+
+                    [Test]
+                    public void AddBefore_NullAsLinkedListNodeWithValue_ThrowsArgumentNullException()
+                    {
+                        list.AddLast(1);
+
+                        Assert.Throws<ArgumentNullException>(() => list.AddBefore(null, 1));
+                    }
+
+                    [Test]
+                    public void AddBefore_WrongLinkedListNodeWithValue_ThrowsInvalidOperationException()
+                    {
+                        LinkedListNode<int> node = new LinkedListNode<int>(1);
+
+                        list.AddLast(1);
+                        list.AddLast(2);
+                        list.AddLast(3);
+                        list.AddLast(4);
+                        list.AddLast(5);
+
+                        Assert.Throws<InvalidOperationException>(() => list.AddBefore(node, 1));
+                    }
+
+                    [Test]
+                    public void AddBefore_LinkedListNodesAndLinkedListNodes_ReturnsCount()
+                    {
+                        int expected = 10;
+
+                        LinkedListNode<int> node1 = list.AddLast(1);
+                        LinkedListNode<int> node2 = list.AddLast(2);
+                        LinkedListNode<int> node3 = list.AddLast(3);
+                        LinkedListNode<int> node4 = list.AddLast(4);
+                        LinkedListNode<int> node5 = list.AddLast(5);
+
+                        list.AddBefore(node1, new LinkedListNode<int>(1));
+                        list.AddBefore(node2, new LinkedListNode<int>(2));
+                        list.AddBefore(node3, new LinkedListNode<int>(3));
+                        list.AddBefore(node4, new LinkedListNode<int>(4));
+                        list.AddBefore(node5, new LinkedListNode<int>(5));
+
+                        Assert.That(list.Count, Is.EqualTo(expected));
+                    }
+
+                    [Test]
+                    public void AddBefore_NullAsLinkedListNodeWithLinkedListNode_ThrowsArgumentNullException()
+                    {
+                        list.AddLast(1);
+
+                        Assert.Throws<ArgumentNullException>(() => list.AddBefore(null, new LinkedListNode<int>(1)));
+                    }
+
+                    [Test]
+                    public void AddBefore_WrongLinkedListNodeWithLinkedListNode_ThrowsInvalidOperationException()
+                    {
+                        LinkedListNode<int> node = new LinkedListNode<int>(1);
+
+                        list.AddLast(1);
+                        list.AddLast(2);
+                        list.AddLast(3);
+                        list.AddLast(4);
+                        list.AddLast(5);
+
+                        Assert.Throws<InvalidOperationException>(() => list.AddBefore(node, new LinkedListNode<int>(1)));
+                    }
+
+                    #endregion
+
+
+                }
+            }
+        }
+    }
+}
