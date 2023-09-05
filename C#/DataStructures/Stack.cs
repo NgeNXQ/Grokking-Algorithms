27ec290ff92059be50f1366ca7050fbedfb0a194
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2023-09-05 20:54:00 +0300

    Implemented Stack

---
+++ C#/DataStructures/Stack.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("GrokkingAlgorithms_CSharp.UnitTests")]
+
+namespace GrokkingAlgorithms
+{
+    namespace CSharp
+    {
+        namespace DataStructures
+        {
+            internal sealed class Stack<T> : IEnumerable<T>
+            {
+                private const int INITIAL_CAPACITY = 10;
+                private const float CAPACITY_FACTOR = 0.7f;
+
+                private T[] stack;
+                private int count;
+
+                public int Count { get => this.count; }
+
+                public Stack()
+                {
+                    this.stack = new T[Stack<T>.INITIAL_CAPACITY];
+                }
+
+                public Stack(int capacity)
+                { 
+                    this.stack = new T[capacity];
+                }
+
+                public void Push(T item)
+                {
+                    if ((float)this.count / this.stack.Length > Stack<T>.CAPACITY_FACTOR)
+                        Array.Resize(ref this.stack, this.stack.Length * 2);
+
+                    this.stack[this.count++] = item;   
+                }
+
+                public T Pop()
+                {
+                    if (this.count < 1)
+                        throw new InvalidOperationException($"{nameof(Stack<T>)} is empty.");
+
+                    return this.stack[this.count--];
+                }
+
+                public bool TryPop(out T result)
+                {
+                    if (this.count < 1)
+                    {
+                        result = default;
+                        return false;
+                    }
+                    else
+                    {
+                        result = this.stack[this.count--];
+                        return true;
+                    }
+                }
+
+                public T Peek()
+                {
+                    if (this.count < 1)
+                        throw new InvalidOperationException($"{nameof(Stack<T>)} is empty.");
+
+                    return this.stack[this.count - 1];
+                }
+
+                public bool TryPeek(out T result)
+                {
+                    if (this.count < 1)
+                    {
+                        result = default;
+                        return false;
+                    }
+                    else
+                    {
+                        result = this.stack[this.count - 1];
+                        return true;
+                    }
+                }
+
+                public void TrimExcess() => Array.Resize(ref this.stack, this.stack.Length);
+
+                public T[] ToArray()
+                {
+                    int index = 0;
+                    T[] array = new T[this.count];
+
+                    foreach (T item in this)
+                        array[index++] = item;
+
+                    return array;
+                }
+
+                public void Clear()
+                {
+                    this.count = 0;
+                    this.stack = new T[Stack<T>.INITIAL_CAPACITY];
+                }
+
+                public IEnumerator<T> GetEnumerator()
+                {
+                    for (int i = this.count; i >= 0; --i)
+                        yield return this.stack[i];
+                }
+
+                IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+            }
+        }
+    }
+}
---
+++ C#/GrokkingAlgorithms_CSharp.UnitTests/StackTests.cs
@@ -0,0 +1,345 @@
+﻿using NUnit.Framework;
+
+namespace GrokkingAlgorithms_CSharp.UnitTests
+{
+    [TestFixture]
+    internal sealed class StackTests
+    {
+        private Stack<int> stack;
+
+        [SetUp]
+        public void SetUp() => this.stack = new Stack<int>();
+
+        #region Push
+
+        [Test]
+        public void Push_Integer_UpdatesCount()
+        {
+            int expected = 5;
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            int actual = stack.Count;
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        #endregion
+
+        #region Pop
+
+        [Test]
+        public void Pop_Integer_ReturnsItem()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            int expected = 5;
+
+            int actual = stack.Pop();
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Pop_Integer_CountIsCorrect()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            int expected = 4;
+
+            stack.Pop();
+            int actual = stack.Count;
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Pop_Integer_CountEqualsZero()
+        {
+            stack.Push(1);
+
+            int expected = 0;
+
+            stack.Pop();
+            int actual = stack.Count;
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowsInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+
+        #endregion
+
+        #region Peek
+
+        [Test]
+        public void Peek_Integer_ReturnsItem()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            int expected = 5;
+
+            int actual = stack.Peek();
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Peek_Integer_CountIsCorrect()
+        {
+            stack.Push(1);
+
+            int expected = 1;
+
+            stack.Peek();
+            int actual = stack.Count;
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Peek_EmptyStack_ThrowsInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => stack.Peek());
+        }
+
+        #endregion
+
+        #region TryPop
+
+        [Test]
+        public void TryPop_Integer_ReturnsItem()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            
+            int expected = 5;
+
+            stack.TryPop(out int actual);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void TryPop_Integer_CountIsCorrect()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            int expected = 4;
+
+            stack.TryPop(out int _);
+            int actual = stack.Count;
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void TryPop_Integer_CountEqualsZero()
+        {
+            stack.Push(1);
+
+            int expected = 0;
+
+            stack.TryPop(out int _);
+            int actual = stack.Count;
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void TryPop_EmptyStack_ReturnsFalse()
+        {
+            bool expected = false;
+
+            bool actual = stack.TryPop(out int _);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void TryPop_EmptyStack_ReturnsDefault()
+        {
+            int expected = 0;
+
+            stack.TryPop(out int actual);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void TryPop_FilledStack_ReturnsTrue()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            bool expected = true;
+
+            bool actual = stack.TryPop(out int _);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void TryPop_EmptyStack_ReturnsItem()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            int expected = 5;
+
+            stack.TryPop(out int actual);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        #endregion
+
+        #region TryPeek
+
+        [Test]
+        public void TryPeek_Integer_ReturnsItem()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            int expected = 5;
+
+            stack.TryPeek(out int actual);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void TryPeek_Integer_CountIsCorrect()
+        {
+            stack.Push(1);
+
+            int expected = 1;
+
+            stack.TryPeek(out int _);
+            int actual = stack.Count;
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void TryPeek_EmptyStack_ReturnsFalse()
+        {
+            bool expected = false;
+
+            bool actual = stack.TryPeek(out int _);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void TryPeek_EmptyStack_ReturnsDefault()
+        {
+            int expected = 0;
+
+            stack.TryPeek(out int actual);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void TryPeek_FilledStack_ReturnsTrue()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            bool expected = true;
+
+            bool actual = stack.TryPeek(out int _);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void TryPeek_EmptyStack_ReturnsItem()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            int expected = 5;
+
+            stack.TryPeek(out int actual);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        #endregion
+
+        #region ToArray
+
+        [Test]
+        public void ToArray_FilledStack_ReturnsFilledArray()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            int[] expected = new int[5] { 5, 4, 3, 2, 1 };
+
+            int[] actual = stack.ToArray();
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void ToArray_EmptyStack_ReturnsFilledArray()
+        {
+            int[] expected = new int[0] { };
+
+            int[] actual = stack.ToArray();
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        #endregion
+    }
+}
