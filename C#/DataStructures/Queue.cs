4323c35ae41859c1ad33d1bdd525636ece4c40d5
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2023-09-15 03:47:46 +0300

    Implemented Queue

---
+++ C#/DataStructures/Queue.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+
+namespace GrokkingAlgorithms
+{
+    namespace CSharp
+    {
+        namespace DataStructures
+        {
+            internal sealed class Queue<T> : IEnumerable<T>
+            {
+                private const int INITIAL_CAPACITY = 10;
+                private const float CAPACITY_THRESHOLD = 0.7f;
+
+                private T[] queue;
+                private int headIndex;
+                private int tailIndex;
+
+                public int Count { get => this.tailIndex - this.headIndex; }
+
+                public Queue() => this.queue = new T[INITIAL_CAPACITY];
+
+                public Queue(int capacity) => this.queue = new T[capacity];
+
+                public Queue(IEnumerable<T> collection)
+                {
+                    foreach (T item in collection)
+                        this.Enqueue(item);
+                }
+
+                public void Enqueue(T item)
+                {
+                    if (this.headIndex == this.tailIndex)
+                    {
+                        this.headIndex = 0;
+                        this.tailIndex = 0;
+                    }
+
+                    if ((float)this.Count / this.queue.Length > CAPACITY_THRESHOLD)
+                        Array.Resize(ref this.queue, this.queue.Length * 2);
+
+                    this.queue[this.tailIndex++] = item;
+                }
+
+                public T Dequeue()
+                {
+                    if (this.Count < 1)
+                        throw new InvalidOperationException($"{nameof(Queue<T>)} is empty");
+
+                    T result = this.queue[this.headIndex++];
+                    return result;
+                }
+
+                public bool TryDequeue(out T result)
+                {
+                    if (this.Count < 1)
+                    {
+                        result = default;
+                        return false;
+                    }
+                    else
+                    {
+                        result = this.queue[this.headIndex++];
+                        return true;
+                    }
+                        
+                }
+
+                public T Peek()
+                {
+                    if (this.Count < 1)
+                        throw new InvalidOperationException($"{nameof(Queue<T>)} is empty.");
+
+                    return this.queue[this.headIndex];
+                }
+
+                public bool TryPeek(out T result)
+                {
+                    if (this.Count < 1)
+                    {
+                        result = default;
+                        return false;
+                    }
+                    else
+                    {
+                        result = this.queue[this.headIndex];
+                        return true;
+                    }
+                }
+
+                public void Clear()
+                {
+                    this.headIndex = 0;
+                    this.tailIndex = 0;
+                    this.queue = new T[INITIAL_CAPACITY];
+                }
+
+                public T[] ToArray()
+                {
+                    T[] array = new T[this.Count];
+
+                    for (int i = 0; i < this.Count; ++i)
+                        array[i] = this.queue[i];
+
+                    return array;
+                }
+
+                public void TrimExcess()
+                {
+                    if ((float)this.Count / this.queue.Length > CAPACITY_THRESHOLD)
+                        Array.Resize(ref queue, this.Count);
+                }
+
+                public IEnumerator<T> GetEnumerator()
+                {
+                    for (int i = 0; i < this.Count; ++i)
+                        yield return this.queue[i];
+                }
+
+                IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+            }
+        }
+    }
+}
---
+++ C#/GrokkingAlgorithms_CSharp.UnitTests/QueueTests.cs
@@ -0,0 +1,354 @@
+﻿using NUnit.Framework;
+
+namespace GrokkingAlgorithms
+{
+    namespace CSharp
+    {
+        namespace DataStructures
+        {
+            namespace UnitTests
+            {
+                [TestFixture]
+                internal sealed class QueueTests
+                {
+                    private Queue<int> queue;
+
+                    [SetUp]
+                    public void SetUp() => this.queue = new Queue<int>();
+
+                    #region Enqueue
+
+                    [Test]
+                    public void Enqueue_Integer_UpdatesCount()
+                    {
+                        int expected = 5;
+
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        int actual = queue.Count;
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    #endregion
+
+                    #region Dequeue
+
+                    [Test]
+                    public void Dequeue_Integer_ReturnsItem()
+                    {
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        int expected = 1;
+
+                        int actual = queue.Dequeue();
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void Dequeue_Integer_CountIsCorrect()
+                    {
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        int expected = 4;
+
+                        queue.Dequeue();
+                        int actual = queue.Count;
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void Dequeue_Integer_CountEqualsZero()
+                    {
+                        queue.Enqueue(1);
+
+                        int expected = 0;
+
+                        queue.Dequeue();
+                        int actual = queue.Count;
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void Dequeue_EmptyQueue_ThrowsInvalidOperationException()
+                    {
+                        Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+                    }
+
+                    #endregion
+
+                    #region Peek
+
+                    [Test]
+                    public void Peek_Integer_ReturnsItem()
+                    {
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        int expected = 1;
+
+                        int actual = queue.Peek();
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void Peek_Integer_CountIsCorrect()
+                    {
+                        queue.Enqueue(1);
+
+                        int expected = 1;
+
+                        queue.Peek();
+                        int actual = queue.Count;
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void Peek_EmptyQueue_ThrowsInvalidOperationException()
+                    {
+                        Assert.Throws<InvalidOperationException>(() => queue.Peek());
+                    }
+
+                    #endregion
+
+                    #region TryDequeue
+
+                    [Test]
+                    public void TryDequeue_Integer_ReturnsItem()
+                    {
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        int expected = 1;
+
+                        queue.TryDequeue(out int actual);
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void TryDequeue_Integer_CountIsCorrect()
+                    {
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        int expected = 4;
+
+                        queue.TryDequeue(out int _);
+                        int actual = queue.Count;
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void TryDequeue_Integer_CountEqualsZero()
+                    {
+                        queue.Enqueue(1);
+
+                        int expected = 0;
+
+                        queue.TryDequeue(out int _);
+                        int actual = queue.Count;
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void TryDequeue_EmptyQueue_ReturnsFalse()
+                    {
+                        bool expected = false;
+
+                        bool actual = queue.TryDequeue(out int _);
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void TryDequeue_EmptyQueue_ReturnsDefault()
+                    {
+                        int expected = 0;
+
+                        queue.TryDequeue(out int actual);
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void TryDequeue_FilledQueue_ReturnsTrue()
+                    {
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        bool expected = true;
+
+                        bool actual = queue.TryDequeue(out int _);
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void TryDequeue_FilledQueue_ReturnsItem()
+                    {
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        int expected = 1;
+
+                        queue.TryDequeue(out int actual);
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    #endregion
+
+                    #region TryPeek
+
+                    [Test]
+                    public void TryPeek_Integer_ReturnsItem()
+                    {
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        int expected = 1;
+
+                        queue.TryPeek(out int actual);
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void TryPeek_Integer_CountIsCorrect()
+                    {
+                        queue.Enqueue(1);
+
+                        int expected = 1;
+
+                        queue.TryPeek(out int _);
+                        int actual = queue.Count;
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void TryPeek_EmptyQueue_ReturnsFalse()
+                    {
+                        bool expected = false;
+
+                        bool actual = queue.TryPeek(out int _);
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void TryPeek_EmptyQueue_ReturnsDefault()
+                    {
+                        int expected = 0;
+
+                        queue.TryPeek(out int actual);
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void TryPeek_FilledQueue_ReturnsTrue()
+                    {
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        bool expected = true;
+
+                        bool actual = queue.TryPeek(out int _);
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    [Test]
+                    public void TryPeek_FilledQueue_ReturnsItem()
+                    {
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        int expected = 1;
+
+                        queue.TryPeek(out int actual);
+
+                        Assert.That(expected, Is.EqualTo(actual));
+                    }
+
+                    #endregion
+
+                    #region ToArray
+
+                    [Test]
+                    public void ToArray_FilledQueue_ReturnsFilledArray()
+                    {
+                        queue.Enqueue(1);
+                        queue.Enqueue(2);
+                        queue.Enqueue(3);
+                        queue.Enqueue(4);
+                        queue.Enqueue(5);
+
+                        int[] expected = new int[5] { 1, 2, 3, 4, 5 };
+
+                        int[] actual = queue.ToArray();
+
+                        CollectionAssert.AreEqual(expected, actual);
+                    }
+
+                    [Test]
+                    public void ToArray_EmptyQueue_ReturnsFilledArray()
+                    {
+                        int[] expected = new int[0] { };
+
+                        int[] actual = queue.ToArray();
+
+                        CollectionAssert.AreEqual(expected, actual);
+                    }
+
+                    #endregion
+                }
+            }
+        }
+    }
+}
